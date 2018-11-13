4d8e5a24638b87a322df431978084c6094bcda73
anjelikaangosta <44987565+anjelikaangosta@users.noreply.github.com>
2018-11-12 19:04:41 -0800

    Update makeamove.py

--- makeamove.py
+++ makeamove.py
@@ -3,6 +3,6 @@ from graphics import *
 def create_window():
     win = GraphWIn()
     win.setCoords (-6, -6, 6, 6)
-    win.setBackground
+    win.setBackground ("green")
     p = Point (0,0)
     p.draw(win)
